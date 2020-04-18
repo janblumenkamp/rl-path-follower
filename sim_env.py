4ba1b3469d6884c5b3b529a654d08c67ac43a5a0
Jan Blumenkamp <jb2270@cam.ac.uk>
2020-04-18 13:24:40 +0200

    New drone model, state contains full path preview

--- sim_env.py
+++ sim_env.py
@@ -4,15 +4,16 @@ import pybullet as p
 import pybullet_data
 from gym.spaces import Box
 from pybullet_multicopter.copters.quadcopter import Quadcopter
+#from sim_env_nodrone import Quadcopter
 from path_generator import PathGenerator
 
 class SimEnv(gym.Env):
     def __init__(self, config):
         self.cfg = config
         self.action_space = Box(-1, 1, shape=(3,), dtype=np.float32)
-        self.observation_space = Box(-np.inf, np.inf, shape=(6,), dtype=np.float32) # orientation, relative current destination, next destination
+        self.observation_space = Box(-np.inf, np.inf, shape=(3*self.cfg['path_state_waypoints_lookahead'],), dtype=np.float32)
         self.client = p.connect(p.GUI if self.cfg['render'] else p.DIRECT)
-        p.setGravity(0, 0, -10, physicsClientId=self.client) 
+        p.setGravity(0, 0, -10, physicsClientId=self.client)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         self.plane_id = p.loadURDF("plane.urdf", physicsClientId=self.client)
 
@@ -24,19 +25,30 @@ class SimEnv(gym.Env):
     def __del__(self):
         p.disconnect()
 
+    def get_simple_rotation_matrix(self):
+        s, c = np.sin(self.drone.orientation_euler[2]), np.cos(self.drone.orientation_euler[2])
+        return np.array([
+            [c, -s, 0],
+            [s, c, 0],
+            [0, 0, 1]])
+
+    def map_action(self, value, out_min, out_max):
+        return (value + 1) * (out_max - out_min) / 2 + out_min
+
     def get_drone_pose(self):
         return np.array(list(self.drone.position)+[self.drone.orientation_euler[2]])
 
     def _reset(self):
         self.drone.reset()
+        self.waypoints_lookahead_amount = self.cfg['path_state_waypoints_lookahead'] * self.cfg['waypoints_lookahead_skip']
         while True:
             self.waypoints = self.path_generator.get_path(self.get_drone_pose())
-            if len(self.waypoints) >= self.cfg['ep_end_after_n_waypoints']:
-                self.waypoints = self.waypoints[:self.cfg['ep_end_after_n_waypoints']+1]
+            if len(self.waypoints) >= self.cfg['ep_end_after_n_waypoints']+self.waypoints_lookahead_amount:
+                self.waypoints = self.waypoints[:self.cfg['ep_end_after_n_waypoints']+self.waypoints_lookahead_amount]
                 break
         self.current_waypoint_index = 0
-        self.next_waypoint_index = 1
         self.timestep = 0
+        self.prev_action = np.array([0,0,0])
         self.last_timestep_new_waypoint = 0
 
     def reset(self):
@@ -44,22 +56,21 @@ class SimEnv(gym.Env):
         return self.step([0,0,0])[0]
 
     def step(self, action):
-        assert(not any(np.isnan(action)))
-        action *= np.array([1, 0.5, 1])
+        assert(not any(np.isnan(action)) and all(np.array(action) <= 1) and all(np.array(action) >= -1))
+        u = self.map_action(action[0], -5, 5)
+        w = self.map_action(action[1], -np.pi, np.pi)
+        h = self.map_action(action[2], -2, 2)
         self.timestep += 1
 
-        self.drone.step_speed(action[0], 0, action[1])
-        self.drone.set_yaw(action[2])
+        self.drone.step_speed(u, 0, w)
+        self.drone.set_yaw_rate(h)
+        self.drone.step_angle()
         self.drone.step()
         p.stepSimulation(physicsClientId=self.client)
 
         reward = 0
         current_waypoint_rel = (self.waypoints[self.current_waypoint_index] - self.drone.position)
-        self.next_waypoint_index = self.current_waypoint_index
-        if self.next_waypoint_index < len(self.waypoints)-1:
-            self.next_waypoint_index += 1
-        next_waypoint_rel = (self.waypoints[self.next_waypoint_index] - self.drone.position)
-        
+
         dist_to_current = np.linalg.norm(current_waypoint_rel, ord=2)
         done = dist_to_current > self.cfg['dist_waypoint_abort_ep'] or (self.timestep - self.last_timestep_new_waypoint) > self.cfg['max_timesteps_between_checkpoints'] or self.drone.position[2] < self.cfg['minimum_drone_height']
         if dist_to_current < self.cfg['dist_waypoint_proceed']:
@@ -69,19 +80,15 @@ class SimEnv(gym.Env):
                 self.current_waypoint_index += 1
             else:
                 done = True
-        m = self.drone.get_rotation_matrix()
-        state = np.concatenate([
-            #np.array([[np.sin(o), np.cos(o)] for o in [self.drone.orientation_euler[2]]]).flatten(),
-            #current_waypoint_rel,
-            #next_waypoint_rel,
-            current_waypoint_rel @ m,
-            next_waypoint_rel @ m,
-            #self.drone.absolute_speed,
-            #self.drone.lateral_speed,
-            #self.drone.ang_speed,
-            #self.drone.position,
-            #[self.drone.thrust, self.drone.setpoint_pitch, self.drone.setpoint_roll, self.drone.setpoint_yaw]
-        ], axis=0)
+        m = self.get_simple_rotation_matrix() #self.drone.get_rotation_matrix()
+        state = np.tensordot(
+            self.waypoints[
+                self.current_waypoint_index
+                :self.current_waypoint_index+self.waypoints_lookahead_amount
+                :self.cfg['waypoints_lookahead_skip']
+            ] - self.drone.position, m, axes=([1],[0])
+        ).flatten()
+        self.prev_action = action
         return state, reward, done, {}
 
     def render(self, _=None):
@@ -100,23 +107,27 @@ class FeedbackNormalizedSimEnv(SimEnv):
 
     def reset(self):
         super()._reset()
-        return self.step([1,0])[0]
+        self.prev_action = np.array([1,0])
+        return self.step([1,0, 0])[0]
 
     def step(self, action):
-        epsilon = (action[0] + 1.0)/2 + 0.001 # [0.001, 1.001]
-        gamma = (action[1] + 1) * 3 # [0, 6]
-        assert(epsilon >= 0.001 and epsilon <= 1.001)
-        assert(gamma >= 0.0 and gamma <= 6.0)
+        epsilon = self.map_action(action[0], 0.1, 1.5)
+        gamma = self.map_action(action[1], 0.1, 10.0) # (action[1] + 1.0)/10 + 0.1 # [0.1, 2.1]
+        kappa_h = 1#mapping(action[1], -0.5, 0.5)
+        #assert(epsilon >= 0.05 and epsilon <= self.cfg['dist_waypoint_proceed']+0.05)
+        #assert(gamma >= 0.1 and gamma <= 2.1)
+        #assert(kappa_h >= -1 and kappa_h <= 1)
 
         orientation = self.drone.orientation_euler[2]
-        next_pos = (self.waypoints[self.next_waypoint_index] - self.drone.position)
-        
+        next_pos = (self.waypoints[self.current_waypoint_index] - self.drone.position)
         position = np.array([
             epsilon * np.cos(orientation),
             epsilon * np.sin(orientation),
-            0], dtype=np.float32)
-        v = (next_pos - position)*gamma
+        ], dtype=np.float32)
+        v = (next_pos[:2] - position)*gamma
+        h = next_pos[2]*kappa_h
         u, w = self.feedback_linearized(orientation, v, epsilon=epsilon)
-        h = v[2]
-
-        return super().step(np.array([u, h, w]))
+        state, reward, done, _ = super().step(np.array([u, h, w]))
+        self.prev_action = action
+        #state = np.array([np.linalg.norm(state[:3]), np.linalg.norm(state[3:6])])
+        return state, reward, done, {}
